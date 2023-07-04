d3946fa0b8d4826f69de6dd340a8a16932abfa6d
Leonardo Valcarenghi <leonardo.valcarenghi@hotmail.com>
2023-07-04 12:22:17 -0300

    menu de cadastro de produto - front

--- Front-End/src/Components/NavBar.jsx
+++ Front-End/src/Components/NavBar.jsx
@@ -1,4 +1,20 @@
 export default function NavBar(props) {
+
+    const { isCompany } = props;
+    const { confirm } = window;
+
+    /**
+     * Fazer logout do usuário.
+     */
+    function Logout() {
+        const confirmLogout = confirm("SAIR DO SISTEMA\nTem certeza que deseja sair?");
+        if (confirmLogout) {
+            delete localStorage["authorization"];
+            window.location.href = "/";
+        }
+    }
+
+
     return (
         <>
             <nav className="navbar navbar-expand-lg bg-primary navbar-dark">
@@ -19,9 +35,25 @@ export default function NavBar(props) {
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+
+                            {/* Exibir somente se login for de empresa. */}
+                            {
+                                isCompany &&
+                                <>
+                                    <li class="nav-item dropdown">
+                                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                            Gerenciar Produtos
+                                        </a>
+                                        <ul class="dropdown-menu">
+                                            <li><a class="dropdown-item px-5" href="/produtos/cadastro">Cadastrar Produto</a></li>
+                                        </ul>
+                                    </li>
+                                </>
+                            }
+
                         </ul>
                         <form className="d-flex">
-                            <button className="btn btn-outline-light px-3 me-2" type="button">
+                            <button className="btn btn-outline-light px-3 me-2" type="button" onClick={Logout}>
                                 <i class="bi bi-door-closed-fill me-2"></i>
                                 Sair
                             </button>
