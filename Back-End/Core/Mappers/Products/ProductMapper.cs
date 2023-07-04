ab08f63287b74af8e34dc78b86b8f1d53a285e74
Leonardo Valcarenghi <leonardo.valcarenghi@hotmail.com>
2023-07-04 12:21:28 -0300

    mapper produtos

---
+++ Back-End/Core/Mappers/Products/ProductMapper.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dietbox.ECommerce.Core.Mappers.Products
+{
+    public class ProductMapper
+    {
+        public void Mapper(IMapperConfigurationExpression mapper)
+        {
+        
+        }
+    }
+}
