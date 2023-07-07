d057b5c977e0d29d4e0b744cb1477bcf7291531d
Leonardo Valcarenghi <leonardo.valcarenghi@hotmail.com>
2023-07-06 21:19:09 -0300

    melhorias na navbar

--- Front-End/src/Components/NavBar.jsx
+++ Front-End/src/Components/NavBar.jsx
@@ -1,6 +1,5 @@
-export default function NavBar(props) {
+export default function NavBar({ isCompany, entityName }) {
 
-    const { isCompany } = props;
     const { confirm } = window;
 
     /**
@@ -17,10 +16,16 @@ export default function NavBar(props) {
 
     return (
         <>
-            <nav className={"navbar navbar-expand-lg  navbar-dark " + (isCompany ? "bg-success" : "bg-primary")}>
+            <nav className={"navbar navbar-expand-lg  navbar-dark fixed-top " + (isCompany ? "bg-success" : "bg-primary")}>
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="/">
-                        Dietbox e-Commerce {isCompany && <small>( Admin )</small>}
+                    <a className="navbar-brand py-0" href="/">
+                        Dietbox e-Commerce
+                        {
+                            isCompany &&
+                            <small className="d-block fst-italic" style={{ fontSize: "12px", marginTop: "-6px" }}>
+                                Admin
+                            </small>
+                        }
                     </a>
                     <button
                         className="navbar-toggler"
@@ -34,30 +39,50 @@ export default function NavBar(props) {
                         <span className="navbar-toggler-icon" />
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+
+                            <li className="nav-item">
+                                <a className="nav-link text-white" href="/">
+                                    <i className="bi bi-house-door-fill me-1" />
+                                    Início
+                                </a>
+                            </li>
 
                             {/* Exibir somente se login for de empresa. */}
                             {
                                 isCompany &&
                                 <>
-                                    <li class="nav-item dropdown">
-                                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                            Gerenciar Produtos
+                                    <li className="nav-item dropdown">
+                                        <a className="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                            <i className="bi bi-box-seam-fill me-1" />
+                                            Produtos
                                         </a>
-                                        <ul class="dropdown-menu">
-                                            <li><a class="dropdown-item px-5" href="/produtos/cadastro">Cadastrar Produto</a></li>
+                                        <ul className="dropdown-menu dropdown-menu-end">
+                                            <li><a className="dropdown-item pe-5" href="/produtos/cadastro">
+                                                <i className="bi bi-plus-square me-2"></i>
+                                                Cadastrar Produto
+                                            </a></li>
                                         </ul>
                                     </li>
                                 </>
                             }
 
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    <i className={"bi me-1 " + (isCompany ? "bi-building-fill" : "bi-person-fill")} />
+                                    {entityName}
+                                </a>
+                                <ul className="dropdown-menu dropdown-menu-end">
+                                    <li>
+                                        <a className="dropdown-item" href="javascript:;" onClick={Logout}>
+                                            <i className="bi bi-door-closed me-2"></i>
+                                            Sair
+                                        </a>
+                                    </li>
+                                </ul>
+                            </li>
+
                         </ul>
-                        <form className="d-flex">
-                            <button className="btn btn-outline-light px-3 me-2" type="button" onClick={Logout}>
-                                <i class="bi bi-door-closed-fill me-2"></i>
-                                Sair
-                            </button>
-                        </form>
                     </div>
                 </div>
             </nav>
